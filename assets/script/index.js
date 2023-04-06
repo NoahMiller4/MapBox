c0cf95e7b05a5f8810c27757b1839dc0de20207f
Noah Miller <Noseby@live.ca>
2023-04-06 13:09:05 -0500

    almost done

--- assets/script/index.js
+++ assets/script/index.js
@@ -9,5 +9,28 @@
 'use strict'
 
 
-const location = document.querySelector('.location')
+const locat = document.querySelector('.location');
+const mapBox = document.querySelector('.map');
 
+function getLocation(position) {
+    console.log(position);
+    const {latitude, longitude } = position.coords;
+
+    locat.addEventListener('click', () => {
+        map.flyTo({
+            center: [longitude, latitude],
+            essential: true
+        })
+    })
+}
+
+function errorHandler() {
+    mapBox.innerHTML = "Cannot display your location"
+    mapBox.style.height = "0"
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getLocation, errorHandler);
+} else {
+    mapBox.innerHTML =  "Geolocation is not supported by your browser"
+}
